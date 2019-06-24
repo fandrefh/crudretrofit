76ab0b2159ccaf17bcfd51397a13e94b49127e7a
Francisco André <fandrefh@gmail.com>
2019-06-23 21:19:23 -0300

    Autenticação

--- crudretrofit/settings.py
+++ crudretrofit/settings.py
@@ -130,12 +130,12 @@ STATIC_URL = '/static/'
 
 # DJANGO REST FRAMEWORK CONFIGURATIONS
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-}
+#REST_FRAMEWORK = {
+#    'DEFAULT_AUTHENTICATION_CLASSES': (
+#        'rest_framework_simplejwt.authentication.JWTAuthentication',
+#    )
+#}
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
-}
+#SIMPLE_JWT = {
+#    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+#}
