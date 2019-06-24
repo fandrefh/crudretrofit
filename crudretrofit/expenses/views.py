246f85de9362dae331b491442d0cc6317057d47f
Francisco André <fandrefh@gmail.com>
2019-06-24 13:51:24 -0300

    Persistência categorias

--- crudretrofit/expenses/views.py
+++ crudretrofit/expenses/views.py
@@ -21,7 +21,11 @@ class CategoryListCreateView(APIView):
         return Response(categories)
     
     def post(self, request, format=None):
-        pass
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
